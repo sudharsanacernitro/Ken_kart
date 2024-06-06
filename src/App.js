257c0f97b980fb48e5dca0a2f793a7267b3dd0ef
sudharsanacernitro <sudharsanr.22cse@kongu.edu>
2024-06-06 18:15:26 +0530

    Commit 1

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Login from './components/login';
+import Main from './components/main';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Main />} />
         </Routes>
       </div>
     </Router>
---
+++ src/components/main.js
@@ -0,0 +1,14 @@
+import { useEffect,useState } from 'react';
+import './login.css';
+import { useNavigate } from 'react-router-dom';
+import Header from './base_components/header';
+function Main()
+{
+    return (
+        <>
+        <Header></Header>
+        </>
+    );
+}
+
+export default Main;
